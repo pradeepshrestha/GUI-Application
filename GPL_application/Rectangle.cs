e2c3293f327bc8e69ccc7bdb81006ef8e6b54db2
pradeepshrestha <pradip.shrestha385@gmail.com>
2019-06-29 10:07:22 +0545

    Second Commit
    Second Commit

---
+++ GPL_application/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GPL_application
+{
+    /// <summary>
+    /// Rectangle class which extends the Shapes class
+    /// </summary>
+    class Rectangle : Shapes
+    {
+        int x, y, size1, size2;
+        Color col;
+        public override void draw(Graphics g)
+        {
+            Pen p = new Pen(col, 2);
+            g.DrawRectangle(p, x, y, size1, size2);
+        }
+        public override void set(Color col,params int[] list)
+        {
+            this.col = col;
+            this.x = list[0];
+            this.y = list[1];
+            this.size1 = list[2];
+            this.size2 = list[3];
+        }
+    }
+}
